59d9ba2773f8c580222f6870868c425acac09949
IvoryCandy <deovant@gmail.com>
2017-10-14 12:03:53 -0400

    reinitialize app

---
+++ HachCat/app/src/main/java/com/orz/hachcat/controller/MainActivity.java
@@ -0,0 +1,15 @@
+package com.orz.hachcat.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.orz.hachcat.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
