7f68ebf7990d2fbb4bf6f5a578e656021a52002b
jzhang866 <stellazhang1118@gmail.com>
2017-10-14 13:48:07 -0400

    voice 25%

--- HackCat/app/src/main/java/com/orz/hachcat/controller/MainActivity.java
+++ HackCat/app/src/main/java/com/orz/hachcat/controller/MainActivity.java
@@ -1,30 +1,40 @@
 package com.orz.hachcat.controller;
 
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
         import android.net.Uri;
         import android.os.Bundle;
-        import android.support.annotation.NonNull;
+import android.speech.RecognizerIntent;
+import android.support.annotation.NonNull;
         import android.support.v7.app.AppCompatActivity;
         import android.util.Log;
-        import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
         import android.widget.Button;
         import android.widget.ImageView;
-        import android.widget.Toast;
+import android.widget.TextView;
+import android.widget.Toast;
 
-        import com.google.android.gms.tasks.OnFailureListener;
-        import com.google.android.gms.tasks.OnSuccessListener;
-        import com.google.firebase.storage.FirebaseStorage;
-        import com.google.firebase.storage.OnProgressListener;
-        import com.google.firebase.storage.StorageReference;
-        import com.google.firebase.storage.UploadTask;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
 import com.orz.hachcat.R;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    protected static final int RESULT_SPEECH = 1;
     private ImageView imageView;
-    private Button btnChoose, btnUpload;
+    private TextView txtText;
+    private Button btnChoose, btnUpload, voice_button;
     private static final int SELECT_PHOTO = 100;
     Uri selectedImage;
     FirebaseStorage storage;
@@ -40,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.imageView);
         btnChoose = (Button) findViewById(R.id.button_choose);
         btnUpload = (Button) findViewById(R.id.button_upload);
+        voice_button = (Button) findViewById(R.id.voice_button);
+        txtText = (TextView) findViewById(R.id.textView2);
         storage = FirebaseStorage.getInstance();
         storageRef = storage.getReference();
 
@@ -57,6 +69,25 @@ public class MainActivity extends AppCompatActivity {
                 uploadImage(imageView);
             }
         });
+        voice_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+
+                try {
+                    startActivityForResult(intent, RESULT_SPEECH);
+                    txtText.setText("");
+                } catch (ActivityNotFoundException a) {
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Opps! Your device doesn't support Speech to Text",
+                            Toast.LENGTH_SHORT);
+                    t.show();
+                }
+            }
+        });
     }
 
     public void imageBrowse(View view) {
@@ -66,14 +97,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
-        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         switch (requestCode) {
             case SELECT_PHOTO:
                 if (resultCode == RESULT_OK) {
-                    selectedImage = imageReturnedIntent.getData();
+                    selectedImage = data.getData();
                     imageView.setImageURI(selectedImage);
                 }
+            case RESULT_SPEECH: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> text = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                    txtText.setText(text.get(0));
+                }
+            }
         }
     }
 
@@ -103,4 +143,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
 }
